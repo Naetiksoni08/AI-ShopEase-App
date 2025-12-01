dc02a7dec414517b427ced89d5d608ec1e62d0b2
Naitikk <Naetiksoni.08@gmail.com>
2025-12-01 19:16:55 +0530

    Deploying Frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
   if (isMaintenance) {
     return (
-      <div className="flex items-center justify-center h-screen text-2xl md:text-3xl font-semibold text-gray-200 bg-dark">
+      <div className="flex items-center justify-center h-screen text-2xl md:text-3xl font-semibold text-gray-200 bg-black">
         🚧 We’re updating things. Back Shortly! 🚧
       </div>
     );
